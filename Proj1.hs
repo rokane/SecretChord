b37fe9df98922cf8551b50a061974bf1a77cb773
rokane <rokane@student.unimelb.edu.au>
2015-09-09 22:33:41 +1000

    Documentation added

--- Proj1.hs
+++ Proj1.hs
@@ -8,25 +8,29 @@ module Proj1 (initialGuess, nextGuess, GameState) where
 
     -- Returns the initial guess and initial GameState with all possible guesses
     initialGuess :: ([String],GameState)
-    initialGuess = (initialGuess, initialState) where
-        initialGuess = ["A1", "B2", "C3"]
-        initialState = removeGuess initialGuess possibleGuesses
+    initialGuess = (initialGuess, initialState) 
+        where
+            initialGuess = ["A3", "G2", "D1"]
+            initialState = removeGuess initialGuess possibleGuesses
 
     -- Returns the next guess.
     -- Takes in previous guess, GameState and feedback from state and uses it
     -- To decide what to eliminate from possible guesses
     nextGuess :: ([String],GameState) -> (Int,Int,Int) -> ([String],GameState) 
-    nextGuess (guess, gameState) feedback = (newState !! ((length newState) `div` 2), newState) where
-        newState = removeIncompatible (guess, gameState) feedback
+    nextGuess (guess, gameState) feedback = (newState !! ((length newState) `div` 2), newState) 
+        where
+            newState = removeIncompatible (guess, gameState) feedback
 
     -- Removes any incompatible guesses from the GameState given the feedback
     -- Checks remaining guesses to see if feedback recieved is the same
+    -- If feedback is the same then the guess is still a valid answer
+    -- Used hint 2 to come up with this method for eliminating guesses
     removeIncompatible :: ([String], GameState) -> (Int,Int,Int) -> GameState
     removeIncompatible (_, []) (_,_,_) = []
     removeIncompatible (guess, x:xs) (p, n, o)
         | p == p' && n == n' && o == o' = x : removeIncompatible (guess, xs) (p, n, o)
         | otherwise                     = removeIncompatible (guess, xs) (p, n, o)
-        where (p', n', o') = response guess x
+        where (p', n', o') = getFeedback guess x
 
     cartProd xs ys = [(x:y) | x <- xs, y <- ys]
 
@@ -35,41 +39,79 @@ module Proj1 (initialGuess, nextGuess, GameState) where
     sublists (x:xs) n = sublists xs n ++ map (x:) (sublists xs $ n - 1)
 
     -- Returns all guess that are possible in the game
+    -- Takes cartesian product of 2 given lists and then maps all possible combinations
     -- Total of 1300 guesses
     possibleGuesses :: [[String]]
     possibleGuesses = sublists (cartProd ['A'..'G'] ["1","2","3"]) 3
 
     -- Removes a guess from the GameState so you can't pick it again
-    removeGuess _ [] = []
+    removeGuess :: (Eq a) => a -> [a] -> [a]
+    removeGuess _ []                    = []
     removeGuess x (y:ys)    | x == y    = removeGuess x ys
                             | otherwise = y: removeGuess x ys
 
-    -- Need to re write this function so that it is my own
-    response :: [String] -> [String] -> (Int,Int,Int)
-    response target guess = (right, rightNote, rightOctave)
-        where   guess'      = nub guess
-                right       = length $ intersect guess' target
-                num         = length guess'
-                rightNote   = num - (length $ deleteFirstsBy (eqNth 0) guess' target) 
-                            - right
-                rightOctave = num - (length $ deleteFirstsBy (eqNth 1) guess' target) 
-                            - right
+    -- Returns feedback corresponding between a guess and a target
+    -- Used to compare the feedback recieved to the feedback from last guess
+    -- If both feedbacks are the same, then the potential guess remains valid
+    getFeedback :: [String] -> [String] -> (Int,Int,Int)
+    getFeedback target guess    = (equalPitch, equalNote, equalOctave)
+        where   equalPitch      = correctPitch guess target
+                equalNote       = correctNote guess target - equalPitch
+                equalOctave     = correctOctave guess target - equalPitch
 
+    -- Returns number of correct pitches between target and guess
     correctPitch :: [String] -> [String] -> Int
-    correctPitch [] _ = 0
-    correctPitch x:xs target
+    correctPitch [] _       = 0
+    correctPitch (x:xs) target
         | x `elem` target   = 1 + correctPitch xs target
         | otherwise         = correctPitch xs target
 
-    -- | eqNth n l1 l2 returns True iff element n of l1 is equal to 
-    --   element n of l2.
-    eqNth :: Eq a => Int -> [a] -> [a] -> Bool
-    eqNth n l1 l2 
-        | (l1 !! n) == (l2 !! n) = True
-        | otherwise = False
+    -- Returns number of correct notes between target and guess
+    -- Minuses the number of correct pitches from final result
+    correctNote :: [String] -> [String] -> Int
+    correctNote [] _            = 0
+    correctNote guess target    = (length guess) - (length $ deleteFirstsBy (compElem 0) guess target)
+
+    -- Returns number of correct octaves between target and guess
+    -- Minuses the number of correct pitches from final result
+    correctOctave :: [String] -> [String] -> Int
+    correctOctave [] _          = 0
+    correctOctave guess target  = (length guess) - (length $ deleteFirstsBy (compElem 1) guess target)
+
+    -- Compares a particular element in two lists
+    -- True iff element x of list1 is equal to element x of list2
+    compElem :: Eq a => Int -> [a] -> [a] -> Bool
+    compElem x list1 list2 
+        | (list1 !! x) == (list2 !! x)  = True
+        | otherwise                     = False
+
+    --intersectExact :: (Eq a) => [a] -> [a] -> [a]
+    --intersectExact a b = nub $ intersect a b
+
+    ---- Returns a list containing the first element of each string in the given list
+    --getFirstElems :: [String] -> [String]
+    --getFirstElems [] = []
+    --getFirstElems (x:xs) = [head x] : getFirstElems xs
+
+    ---- Returns a list containing the last element of each string in the given list
+    --getLastElems :: [String] -> [String]
+    --getLastElems [] = []
+    --getLastElems (x:xs) = tail x : getLastElems xs
+
+    --   -- Returns number of correct notes between target and guess
+    --correctNote1 :: [String] -> [String] -> Int
+    --correctNote1 [] _ = 0
+    --correctNote1 guess target = length $ intersectExact (getFirstElems guess) (getFirstElems target)
+
+    ---- Returns number of correct octaves between target and guess
+    --correctOctave1 :: [String] -> [String] -> Int
+    --correctOctave1 [] _ = 0
+    --correctOctave1 guess target = length $ intersectExact (getLastElems guess) (getLastElems target)
 
 
-    -- Pitch comparison use `elem`
-    -- 
+
+
+
+
 
 
